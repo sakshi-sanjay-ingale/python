80c1a8630c9ff209a0da6e80e82210d36ca000e3
sakshi-sanjay-ingale <ingalesakshi458@gmail.com>
2025-07-31 12:04:51 +0530

    if statement elif and else

---
+++ Score.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your test score: "))
+
+if score >= 50:
+    print("Grade: A")
+elif score >= 45:
+    print("Grade: B")
+elif score >= 40:
+    print("Grade: C")
+else:
+    print("Grade: D or F")
