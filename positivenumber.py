4678194e1a6a8d8835cee04d5b82bc78a64291cc
sanjaysakshiingale <ingalesakshi458@gmail.com>
2025-07-31 10:02:05 +0530

    Add files via upload

---
+++ positivenumber.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+# Check if the number is positive
+if number > 0:
+    print("The number is positive.")
+
+# This line runs no matter what
+print("Program end.")
